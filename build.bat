14d892ee7537c8f3f03e4bcbe6e0468323af9990
Алексей Тропин <ltropin@mail.ru>
2024-01-18 16:06:07 +0500

    Init project

---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+
+flutter pub run pigeon --input pigeons/linkos_sdk.dart
---
+++ pigeons/linkos_sdk.dart
@@ -0,0 +1,44 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/src/messages.g.dart',
+  dartOptions: DartOptions(),
+  cppOptions: CppOptions(namespace: 'pigeon_example'),
+
+  swiftOptions: SwiftOptions(),
+  // Set this to a unique prefix for your plugin or application, per Objective-C naming conventions.
+  objcOptions: ObjcOptions(prefix: 'PGN'),
+  copyrightHeader: 'pigeons/copyright.txt',
+  dartPackageName: 'pigeon_example_package',
+  // Out files
+  // C++
+  cppHeaderOut: 'windows/runner/messages.g.h',
+  cppSourceOut: 'windows/runner/messages.g.cpp',
+  // ObjC
+  objcHeaderOut: 'macos/Runner/messages.g.h',
+  objcSourceOut: 'macos/Runner/messages.g.m',
+  // Java
+  javaOut: 'android/app/src/main/java/io/flutter/plugins/Messages.java',
+))
+enum Code { one, two }
+
+class MessageData {
+  MessageData({required this.code, required this.data});
+  String? name;
+  String? description;
+  Code code;
+  Map<String?, String?> data;
+}
+
+@HostApi()
+abstract class ExampleHostApi {
+  String getHostLanguage();
+
+  // These annotations create more idiomatic naming of methods in Objc and Swift.
+  @ObjCSelector('addNumber:toNumber:')
+  @SwiftFunction('add(_:to:)')
+  int add(int a, int b);
+
+  @async
+  bool sendMessage(MessageData message);
+}
